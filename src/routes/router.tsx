88762ea47ae2d3b5d863b5185ad320febd28f6a6
unknown <sonyk9919@gmail.com>
2022-12-30 21:37:03 +0900

    icon 추가

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,15 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Helmet } from "react-helmet";
 import SideBarContainer from "../Container/SideBarContainer";
 import Etc from "./pages/Etc";
 import Gasture from "./pages/Gasture";
 import Home from "./pages/Home";
 import Layout from "./pages/Layout";
 import Slider from "./pages/Slider";
+import icon from "../Resource/icon/sun.png";
 
 const Router = () => {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <SideBarContainer />
+      <Helmet>
+        <link rel="shortcut icon" href={icon} />
+        <title>My Note</title>
+      </Helmet>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Slider" element={<Slider />} />
